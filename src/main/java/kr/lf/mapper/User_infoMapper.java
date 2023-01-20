feaca2959da84d1b208bae49a2de2067e16d9458
handaechan <h33223012@naver.com>
2023-01-20 11:09:43 +0900

    대찬

--- src/main/java/kr/lf/mapper/User_infoMapper.java
+++ src/main/java/kr/lf/mapper/User_infoMapper.java
@@ -14,7 +14,7 @@ public interface User_infoMapper {
 	@Insert("insert into user_info(user_id,user_pw,user_hp,user_email) values(#{user_id},#{user_pw},#{user_hp},#{user_email})")
 	public int join(User_infoDTO dto);
 	
-	@Select("select * from user_info where user_id =#{user_id} and user_pw = #{user_id}")
+	@Select("select * from user_info where user_id =#{user_id} and user_pw = #{user_pw}")
 	public int login(@Param("user_id") String user_id,@Param("user_pw") String user_pw);
 	
 	@Select("select * from user_info")
